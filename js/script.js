f29f0a2e30599e133f3c7b929b446856cb3d1661
Mateusz Górski <mateusz.gorski1302@gmail.com>
2021-10-26 15:46:07 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -1,7 +1,66 @@
+var  buttonRock, buttonPaper, buttonScissors, argButtonName,
 
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+buttonRock = document.getElementById('button-rock');
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
+
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+
+
+var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+
+/**
+ * Describe this function...
+ */
+function getMoveName(argMoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamien';
+  } else if (argMoveId == 2) {
+    return 'papier';
+  } else if (argMoveId == 3) {
+    return 'nozyczki';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'kamien';
+  }
+}
+
+/**
+ * Describe this function...
+ */
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'papier' && argComputerMove == 'kamien') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'kamien' && argComputerMove == 'nozyczki') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'nozyczki' && argComputerMove == 'papier') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'nozyczki' && argComputerMove == 'nozyczki') {
+    printMessage('remis');
+  } else if (argPlayerMove == 'papier' && argComputerMove == 'papier') {
+    printMessage('remis');
+  } else if (argPlayerMove == 'kamien' && argComputerMove == 'kamien') {
+    printMessage('remis');
+  } else {
+    printMessage('Przegrywasz :(');
+  }
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+}
+playerMove = argButtonName;
+console.log('wybór ruchu gracza to: ' + playerInput);
+playerMove = argButtonName;
+console.log('ruch gracza to: ' + playerMove);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(playerMove, computerMove);
+}
+
+buttonRock.addEventListener('click', function(){ buttonClicked('kamien'); });
+buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
+buttonScissors.addEventListener('click', function(){ buttonClicked('nozyczki'); });
